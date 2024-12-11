d39f4ba28bb7bbc0f979ea7d444892b79c1e6f49
tanveeralam <tan24alam11@gmail.com>
2024-12-11 15:30:05 +0530

    grpc demo app

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # grpc-demo
+
+[ref](https://www.koyeb.com/tutorials/build-a-grpc-api-using-go-and-grpc-gateway)
+
---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+# set -e
+
+echo "=== Insert Test Data ==="
+
+# curl -X POST localhost:8080 -d \
+# '{"activity": {"description": "christmas eve bike class", "time":"2021-12-09T16:34:04Z"}}'
+
+# curl -X POST localhost:8080 -d \
+# '{"activity": {"description": "cross country skiing is horrible and cold", "time":"2021-12-09T16:56:12Z"}}'
+
+# curl -X POST localhost:8080 -d \
+# '{"activity": {"description": "sledding with nephew", "time":"2021-12-09T16:56:23Z"}}'
+
+# curl -X GET localhost:8080 -d '{"id": 1}'
+
+# curl -X GET localhost:8080 -d '{"id": 2}'
+
+# curl -X GET localhost:8080 -d '{"id": 3}'
